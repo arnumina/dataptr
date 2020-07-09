c802bd56d803e6c30dc61fe197360b17a684ff61
Loïc TROCHET <ltrochet@ina.fr>
2020-07-09 19:59:01 +0200

    Ajout de multiples fonctionnalités

--- dataptr.go
+++ dataptr.go
@@ -16,6 +16,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/arnumina/failure"
 	"github.com/dolmen-go/jsonptr"
@@ -23,6 +24,8 @@ import (
 )
 
 var (
+	// ErrBadType AFAIRE.
+	ErrBadType = errors.New("bad type")
 	// ErrNotFound AFAIRE.
 	ErrNotFound = errors.New("not found")
 )
@@ -30,43 +33,45 @@ var (
 type (
 	// DataPtr AFAIRE.
 	DataPtr struct {
-		data interface{}
+		value interface{}
 	}
 )
 
 // New AFAIRE.
-func New(data interface{}) *DataPtr {
-	return &DataPtr{data: data}
+func New(value interface{}) *DataPtr {
+	return &DataPtr{value: value}
 }
 
 // Empty AFAIRE.
 func Empty() *DataPtr {
-	return &DataPtr{data: map[string]interface{}(nil)}
+	return New(map[string]interface{}(nil))
 }
 
 // FromJSON AFAIRE.
 func FromJSON(data []byte) (*DataPtr, error) {
-	dp := &DataPtr{}
-	if err := json.Unmarshal(data, &dp.data); err != nil {
+	var value interface{}
+
+	if err := json.Unmarshal(data, &value); err != nil {
 		return nil, err
 	}
 
-	return dp, nil
+	return New(value), nil
 }
 
 // FromYAML AFAIRE.
 func FromYAML(data []byte) (*DataPtr, error) {
-	dp := &DataPtr{}
-	if err := yaml.Unmarshal(data, &dp.data); err != nil {
+	var value interface{}
+
+	if err := yaml.Unmarshal(data, &value); err != nil {
 		return nil, err
 	}
 
-	return dp, nil
+	return New(value), nil
 }
 
-// Data AFAIRE.
-func (dp *DataPtr) Data() interface{} {
-	return dp.data
+// Value AFAIRE.
+func (dp *DataPtr) Value() interface{} {
+	return dp.value
 }
 
 // Get AFAIRE.
@@ -77,19 +82,19 @@ func (dp *DataPtr) Get(keys ...string) (string, *DataPtr, error) {
 		return ptr, dp, nil
 	}
 
-	d, err := jsonptr.Get(dp.data, ptr)
+	value, err := jsonptr.Get(dp.value, ptr)
 	if err != nil {
 		if errors.Is(err, jsonptr.ErrProperty) {
 			return ptr, nil,
 				failure.New(ErrNotFound).
 					Set("pointer", ptr).
-					Msg("this data does not exist") ////////////////////////////////////////////////////////////////////
+					Msg("the referenced data does not exist") //////////////////////////////////////////////////////////
 		}
 
 		return ptr, nil, err
 	}
 
-	return ptr, &DataPtr{data: d}, nil
+	return ptr, New(value), nil
 }
 
 // MaybeGet AFAIRE.
@@ -104,6 +109,169 @@ func (dp *DataPtr) MaybeGet(keys ...string) (*DataPtr, error) {
 	}
 
 	return dptr, nil
+}
+
+// MapString AFAIRE.
+func (dp *DataPtr) MapString(keys ...string) (map[string]*DataPtr, error) {
+	ptr, dptr, err := dp.Get(keys...)
+	if err != nil {
+		return nil, err
+	}
+
+	ms, ok := dptr.value.(map[string]interface{})
+	if !ok {
+		return nil,
+			failure.New(ErrBadType).
+				Set("pointer", ptr).
+				Msg("this pointer do not reference a value of type 'map[string]interface{}'") //////////////////////////
+	}
+
+	value := make(map[string]*DataPtr)
+
+	for k, v := range ms {
+		value[k] = New(v)
+	}
+
+	return value, nil
+}
+
+// Slice AFAIRE.
+func (dp *DataPtr) Slice(keys ...string) ([]*DataPtr, error) {
+	ptr, dptr, err := dp.Get(keys...)
+	if err != nil {
+		return nil, err
+	}
+
+	slice, ok := dptr.value.([]interface{})
+	if !ok {
+		return nil,
+			failure.New(ErrBadType).
+				Set("pointer", ptr).
+				Msg("this pointer do not reference a value of type '[]interface{}'") ///////////////////////////////////
+	}
+
+	value := []*DataPtr{}
+
+	for _, v := range slice {
+		value = append(value, New(v))
+	}
+
+	return value, nil
+}
+
+// Bool AFAIRE.
+func (dp *DataPtr) Bool(keys ...string) (bool, error) {
+	ptr, dptr, err := dp.Get(keys...)
+	if err != nil {
+		return false, err
+	}
+
+	value, ok := dptr.value.(bool)
+	if !ok {
+		return false,
+			failure.New(ErrBadType).
+				Set("pointer", ptr).
+				Msg("this pointer do not reference a value of type 'bool'") ////////////////////////////////////////////
+	}
+
+	return value, nil
+}
+
+// DBool AFAIRE.
+func (dp *DataPtr) DBool(d bool, keys ...string) (bool, error) {
+	value, err := dp.Bool(keys...)
+	if errors.Is(err, ErrNotFound) {
+		return d, nil
+	}
+
+	return value, err
+}
+
+// Int AFAIRE.
+func (dp *DataPtr) Int(keys ...string) (int, error) {
+	ptr, dptr, err := dp.Get(keys...)
+	if err != nil {
+		return 0, err
+	}
+
+	value, ok := dptr.value.(int)
+	if !ok {
+		return 0,
+			failure.New(ErrBadType).
+				Set("pointer", ptr).
+				Msg("this pointer do not reference a value of type 'int'") /////////////////////////////////////////////
+	}
+
+	return value, nil
+}
+
+// DInt AFAIRE.
+func (dp *DataPtr) DInt(d int, keys ...string) (int, error) {
+	value, err := dp.Int(keys...)
+	if errors.Is(err, ErrNotFound) {
+		return d, nil
+	}
+
+	return value, err
+}
+
+// String AFAIRE.
+func (dp *DataPtr) String(keys ...string) (string, error) {
+	ptr, dptr, err := dp.Get(keys...)
+	if err != nil {
+		return "", err
+	}
+
+	value, ok := dptr.value.(string)
+	if !ok {
+		return "",
+			failure.New(ErrBadType).
+				Set("pointer", ptr).
+				Msg("this pointer do not reference a value of type 'string'") //////////////////////////////////////////
+	}
+
+	return value, nil
+}
+
+// DString AFAIRE.
+func (dp *DataPtr) DString(d string, keys ...string) (string, error) {
+	value, err := dp.String(keys...)
+	if errors.Is(err, ErrNotFound) {
+		return d, nil
+	}
+
+	return value, err
+}
+
+// Duration AFAIRE.
+func (dp *DataPtr) Duration(keys ...string) (time.Duration, error) {
+	ptr, dptr, err := dp.Get(keys...)
+	if err != nil {
+		return 0, err
+	}
+
+	s, ok := dptr.value.(string)
+	if ok {
+		value, err := time.ParseDuration(s)
+		if err == nil {
+			return value, nil
+		}
+	}
+
+	return 0,
+		failure.New(ErrBadType).
+			Set("pointer", ptr).
+			Msg("this pointer do not reference a value of type 'time.Duration'") ///////////////////////////////////////
+}
+
+// DDuration AFAIRE.
+func (dp *DataPtr) DDuration(d time.Duration, keys ...string) (time.Duration, error) {
+	value, err := dp.Duration(keys...)
+	if errors.Is(err, ErrNotFound) {
+		return d, nil
+	}
+
+	return value, err
 }
 
 /*
--- dataptr_test.go
+++ dataptr_test.go
@@ -11,6 +11,213 @@
 
 package dataptr_test
 
+import (
+	"errors"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/arnumina/dataptr"
+)
+
+var (
+	_testData = map[string]interface{}{
+		"x": 785,
+		"y": map[string]interface{}{
+			"foo":     "bar",
+			"ok":      true,
+			"null":    nil,
+			"timeout": "5m",
+			"map": map[string]interface{}{
+				"a": "a",
+				"b": "b",
+				"c": "c",
+			},
+		},
+		"z":     []interface{}{"ok", 2, 5, 7, true, 13, "30s"},
+		"slice": []interface{}{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+	}
+)
+
+func TestFromJSON(t *testing.T) {
+	_, err := dataptr.FromJSON([]byte(`{"x": 785, "y": {"foo": "bar", "ok": true, "null": null}}`))
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+}
+
+func TestFromYAML(t *testing.T) {
+	data := `x: 785
+y:
+    foo: bar
+    ok: true
+    null: null`
+
+	_, err := dataptr.FromYAML([]byte(data))
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+}
+
+func TestMapString(t *testing.T) {
+	_, err := dataptr.New(_testData).MapString("x")
+	if !errors.Is(err, dataptr.ErrBadType) {
+		t.Errorf("Error expected: %s", dataptr.ErrBadType)
+	}
+
+	ms, err := dataptr.New(_testData).MapString("y", "map")
+	if err != nil {
+		t.Errorf("Error: %s", err)
+		return
+	}
+
+	for k, v := range ms {
+		if !reflect.DeepEqual(k, v.Value()) {
+			t.Errorf("got: %v, want %v", v.Value(), k)
+		}
+	}
+}
+
+func TestSlice(t *testing.T) {
+	_, err := dataptr.New(_testData).Slice("y", "foo")
+	if !errors.Is(err, dataptr.ErrBadType) {
+		t.Errorf("Error expected => got: %v want: %v", err, dataptr.ErrBadType)
+	}
+
+	s, err := dataptr.New(_testData).Slice("slice")
+	if err != nil {
+		t.Errorf("Error: %s", err)
+		return
+	}
+
+	for i, v := range s {
+		if !reflect.DeepEqual(i, v.Value()) {
+			t.Errorf("got: %v, want %v", v.Value(), i)
+		}
+	}
+}
+
+func TestBool(t *testing.T) {
+	_, err := dataptr.New(_testData).Bool("x")
+	if !errors.Is(err, dataptr.ErrBadType) {
+		t.Errorf("Error expected: %s", dataptr.ErrBadType)
+	}
+}
+
+func TestDBool(t *testing.T) {
+	dp := dataptr.New(_testData)
+
+	tests := []struct {
+		d    bool
+		want bool
+		keys []string
+	}{
+		{false, true, []string{"y", "ok"}},
+		{false, true, []string{"z", "4"}},
+		{true, true, []string{"y", "ko"}},
+		{false, false, []string{"y", "ko"}},
+	}
+
+	for i, tt := range tests {
+		got, err := dp.DBool(tt.d, tt.keys...)
+		if err != nil {
+			t.Errorf("[%02d] => error: %s", i, err)
+		} else if got != tt.want {
+			t.Errorf("[%02d] => got: %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestInt(t *testing.T) {
+	_, err := dataptr.New(_testData).Int("y", "foo")
+	if !errors.Is(err, dataptr.ErrBadType) {
+		t.Errorf("Error expected: %s", dataptr.ErrBadType)
+	}
+}
+
+func TestDInt(t *testing.T) {
+	dp := dataptr.New(_testData)
+
+	tests := []struct {
+		d    int
+		want int
+		keys []string
+	}{
+		{0, 785, []string{"x"}},
+		{0, 13, []string{"z", "5"}},
+		{123, 123, []string{"y", "ko"}},
+	}
+
+	for i, tt := range tests {
+		got, err := dp.DInt(tt.d, tt.keys...)
+		if err != nil {
+			t.Errorf("[%02d] => error: %s", i, err)
+		} else if got != tt.want {
+			t.Errorf("[%02d] => got: %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestString(t *testing.T) {
+	_, err := dataptr.New(_testData).String("z")
+	if !errors.Is(err, dataptr.ErrBadType) {
+		t.Errorf("Error expected: %s", dataptr.ErrBadType)
+	}
+}
+
+func TestDString(t *testing.T) {
+	dp := dataptr.New(_testData)
+
+	tests := []struct {
+		d    string
+		want string
+		keys []string
+	}{
+		{"", "bar", []string{"y", "foo"}},
+		{"", "ok", []string{"z", "0"}},
+		{"default", "default", []string{"ko"}},
+	}
+
+	for i, tt := range tests {
+		got, err := dp.DString(tt.d, tt.keys...)
+		if err != nil {
+			t.Errorf("[%02d] => error: %s", i, err)
+		} else if got != tt.want {
+			t.Errorf("[%02d] => got: %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
+func TestDuration(t *testing.T) {
+	_, err := dataptr.New(_testData).Duration("y", "ok")
+	if !errors.Is(err, dataptr.ErrBadType) {
+		t.Errorf("Error expected: %s", dataptr.ErrBadType)
+	}
+}
+
+func TestDDuration(t *testing.T) {
+	dp := dataptr.New(_testData)
+
+	tests := []struct {
+		d    time.Duration
+		want time.Duration
+		keys []string
+	}{
+		{0, 5 * time.Minute, []string{"y", "timeout"}},
+		{0, 30 * time.Second, []string{"z", "6"}},
+		{20 * time.Millisecond, 20 * time.Millisecond, []string{"ko", "ok"}},
+	}
+
+	for i, tt := range tests {
+		got, err := dp.DDuration(tt.d, tt.keys...)
+		if err != nil {
+			t.Errorf("[%02d] => error: %s", i, err)
+		} else if got != tt.want {
+			t.Errorf("[%02d] => got: %v, want %v", i, got, tt.want)
+		}
+	}
+}
+
 /*
 ######################################################################################################## @(°_°)@ #######
 */
